8dfe2a6530bf689aff9a98fef507fa7cd088ad85
henry sanchez <hrsd83@gmail.com>
2021-02-02 10:32:49 -0300

    Se le agrega carrito con productos a compra.html actualizado

--- js/animate.js
+++ js/animate.js
@@ -1,12 +1,14 @@
+// MODO DARK 
 function toggleDarkLight() {
   var body = document.getElementById('body');
   var currentClass = body.className;
   body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
-  // body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+  
+
 
 }
 
-
+// ANIMACION DE TARJETAS  
 function myTarjetas() {
   const tarjetas = document.getElementById('items')
   tarjetas.classList.add('magictime', 'spaceInDown')
